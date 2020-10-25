82e03a896fe863068357ea8bc3a48a32a7ba86ce
uaolmer <uaolmer2@gmail.com>
2020-10-25 05:33:45 +0200

    feat: add solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,14 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  
+  const arr = [];
+  let result = undefined;
+
+  str.split('').forEach(char => {
+    char === arr[0] ? arr.shift() : bracketsConfig.filter(e => e.includes(char)) ? 
+      arr.unshift(bracketsConfig.filter(e => e.includes(char))[0][1]) : false;
+
+    result = arr.length === 0;
+  });
+
+  return result;
 }
